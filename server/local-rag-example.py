481f5f070f6377690698687a8dc1be4bf5bddbba
Josh Mabry <mabry1985@gmail.com>
2024-03-29 19:30:54 -0700

    update to use infinity wrapper

--- server/local-rag-example.py
+++ server/local-rag-example.py
@@ -1,49 +1,68 @@
 
 from operator import itemgetter
-
+from langchain_community.embeddings import InfinityEmbeddings, InfinityEmbeddingsLocal
 from langchain_community.vectorstores import FAISS
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings
 from models.chat_model import model
-
-embeddings_model = OpenAIEmbeddings(openai_api_base="http://localhost:5050/v1",
-  openai_api_key="sk-1234567890")
-
-embeddings = embeddings_model.embed_documents(
-    [
-        "Hi there!",
-        "Oh, hello!",
-        "What's your name?",
-        "My friends call me World",
-        "Hello World!"
-    ]
+documents = [
+    "Baguette is a dish.",
+    "Paris is the capital of France.",
+    "numpy is a lib for linear algebra",
+    "You escaped what I've escaped - You'd be in Paris getting fucked up too",
+]
+query = "Where is Paris?"
+infinity_api_url = "http://localhost:7997"
+# model is currently not validated.
+embeddings = InfinityEmbeddings(
+    model="sentence-transformers/all-MiniLM-L6-v2", infinity_api_url=infinity_api_url
 )
-print(embeddings)
-vectorstore = FAISS.from_texts(
-        [
-        "Assisting User David with his academic research on the impact of climate change on marine life. Provided him with multiple resources including relevant scientific papers, news articles, and documentaries.",
-        "Guided User Emma through a detailed step-by-step troubleshooting process to fix a wireless connection issue on her home computer. She was able to complete her online assignment in time for its deadline",
-        "What's your name?",
-        "My friends call me World",
-        "Hello World!"
-    ], embedding=embeddings_model
-)
-retriever = vectorstore.as_retriever()
+try:
+    documents_embedded = embeddings.embed_documents(documents)
+    query_result = embeddings.embed_query(query)
+    print(query_result)
+    print("embeddings created successful")
+except Exception as ex:
+    print(
+        "Make sure the infinity instance is running."
+    )
 
-template = """Answer the question based only on the following context:
-{context}
 
-Question: {question}
-"""
-prompt = ChatPromptTemplate.from_template(template)
+# embeddings = embeddings_model.embed_documents(
+#     [
+#         "Hi there!",
+#         "Oh, hello!",
+#         "What's your name?",
+#         "My friends call me World",
+#         "Hello World!"
+#     ]
+# )
+# print(embeddings)
+# vectorstore = FAISS.from_texts(
+#         [
+#         "Assisting User David with his academic research on the impact of climate change on marine life. Provided him with multiple resources including relevant scientific papers, news articles, and documentaries.",
+#         "Guided User Emma through a detailed step-by-step troubleshooting process to fix a wireless connection issue on her home computer. She was able to complete her online assignment in time for its deadline",
+#         "What's your name?",
+#         "My friends call me World",
+#         "Hello World!"
+#     ], embedding=embeddings_model
+# )
+# retriever = vectorstore.as_retriever()
 
-chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
-    | prompt
-    | model
-    | StrOutputParser()
-)
+# template = """Answer the question based only on the following context:
+# {context}
 
-print(chain.invoke("where did we help Emma do?"))
+# Question: {question}
+# """
+# prompt = ChatPromptTemplate.from_template(template)
+
+# chain = (
+#     {"context": retriever, "question": RunnablePassthrough()}
+#     | prompt
+#     | model
+#     | StrOutputParser()
+# )
+
+# print(chain.invoke("where did we help Emma do?"))
