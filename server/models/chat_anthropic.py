4e8628f32d717abbfb7a938f46b4f7dcb072fce5
Josh Mabry <mabry1985@gmail.com>
2024-04-07 10:32:17 -0700

    add anthropic example

--- server/models/chat_anthropic.py
+++ server/models/chat_anthropic.py
@@ -7,20 +7,20 @@ load_dotenv()
 
 chat_anthropic = ChatAnthropic(temperature=0, model_name="claude-3-opus-20240229")
 
-system = (
-    "You are a helpful assistant that translates {input_language} to {output_language}. Output only the translated text, nothing else."
-)
-human = "{text}"
-prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+# system = (
+#     "You are a helpful assistant that translates {input_language} to {output_language}. Output only the translated text, nothing else."
+# )
+# human = "{text}"
+# prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
 
-chain = prompt | chat_anthropic
-result = chain.invoke(
-    {
-        "input_language": "English",
-        "output_language": "Korean",
-        "text": "나는 크고 뚱뚱한 램프를 사랑해",
-    }
-)
+# chain = prompt | chat_anthropic
+# result = chain.invoke(
+#     {
+#         "input_language": "English",
+#         "output_language": "Korean",
+#         "text": "나는 크고 뚱뚱한 램프를 사랑해",
+#     }
+# )
 
-print(result.content)
+# print(result.content)
 
